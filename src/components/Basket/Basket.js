da29c08d45bd2b12c84c745ad8a9b6145cec72e0
Abidin K <abidinkofficial@gmail.com>
2021-11-06 13:02:04 +0300

    Added initial basket component and initial shaking animation

---
+++ src/components/Basket/Basket.js
@@ -0,0 +1,11 @@
+import styles from "./Basket.module.scss"
+
+const Basket = () => {
+  return (
+    <div className={styles.Basket}>
+      
+    </div>
+  )
+}
+
+export default Basket
--- src/pages/ShakingTree/ShakingTree.js
+++ src/pages/ShakingTree/ShakingTree.js
@@ -1,11 +1,15 @@
 import styles from "./ShakingTree.module.scss"
+import { ReactComponent as Ground } from "./Ground.svg"
 
 import Tree from "../../components/Tree"
+import Basket from "../../components/Basket"
 
 const ShakingTree = () => {
   return (
     <div className={styles.ShakingTree}>
       <Tree />
+      <Basket />
+      <Ground className={styles.Ground} />
     </div>
   )
 }
