e5eae699c15b4765f993dcc0114f440134322cb6
Abidin K <abidinkofficial@gmail.com>
2021-11-06 17:34:45 +0300

    Added initial apple component, improved shaking animation

---
+++ src/components/Apple/Apple.js
@@ -0,0 +1,11 @@
+import styles from "./Apple.module.scss"
+
+const Apple = () => {
+  return (
+    <div className={styles.Apple}>
+
+    </div>
+  )
+}
+
+export default Apple
--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -1,9 +1,11 @@
 import styles from "./Tree.module.scss"
 
+import Apple from "../Apple"
+
 const Tree = () => {
   return (
     <div className={styles.Tree}>
-    
+      <Apple />
     </div>
   )
 }
